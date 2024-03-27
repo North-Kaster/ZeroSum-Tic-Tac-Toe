00eded5dcd610c40b4b87c2ecce42c5272b7562c
North-Kaster <bckaster@bsu.edu>
2024-03-27 10:36:06 -0400

    Add skeleton for tictactoeplayer

--- ZeroSumGame/src/solutions/TicTacToePlayer.java
+++ ZeroSumGame/src/solutions/TicTacToePlayer.java
@@ -1,9 +1,23 @@
-package solutions;
+import core_algorithms.Minimax;
+import problems.TicTacToe;
 
-public class TicTacToePlayer {
-    //while we have not reached a terminal state:
-         //print the current board
-         //let the human player input (row, column)
-        //run the minimax search to find the best move
-    //announce the winner or draw
+public class TicTacToePlayer extends Minimax<char[][], int[]> {
+
+    public TicTacToePlayer(int size) {
+        super(new TicTacToe(size, TicTacToe.Marks.O), false);
+    }
+    public void play(){
+        // LOOP: while we have not reached a terminal state:
+            // Print current gameboard
+            // Have AI choose starting spot
+            // Print current gameboard
+            // Ask for user input row# and column#. Format as (row, column)?
+            // run the minimax search to find the best move
+        //announce the winner or draw
+    }
+
+    public static void main(String[] args) {
+        TicTacToePlayer player = new TicTacToePlayer(3); // Choose size of board
+        player.play();
+    }
 }
