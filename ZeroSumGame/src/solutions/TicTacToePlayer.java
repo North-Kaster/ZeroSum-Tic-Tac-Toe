f0ee868c6117516d6253c805d1f7f2067901ef54
jeorozc0 <jeorozco@bsu.edu>
2024-03-27 19:30:58 -0400

    add board printing and AI move

--- ZeroSumGame/src/solutions/TicTacToePlayer.java
+++ ZeroSumGame/src/solutions/TicTacToePlayer.java
@@ -1,3 +1,5 @@
+package solutions;
+
 import core_algorithms.Minimax;
 import problems.TicTacToe;
 
@@ -6,18 +8,59 @@ public class TicTacToePlayer extends Minimax<char[][], int[]> {
     public TicTacToePlayer(int size) {
         super(new TicTacToe(size, TicTacToe.Marks.O), false);
     }
-    public void play(){
-        // LOOP: while we have not reached a terminal state:
-            // Print current gameboard
-            // Have AI choose starting spot
-            // Print current gameboard
-            // Ask for user input row# and column#. Format as (row, column)?
-            // run the minimax search to find the best move
-        //announce the winner or draw
+
+    public void play() {
+        TicTacToe game = new TicTacToe(3, TicTacToe.Marks.X);
+        char[][] board = new char[3][3];
+        int x = 0;
+
+        while (x == 0) {
+            printBoard(board);
+            int[] aiMove = minimaxSearch(board);// Use minimaxSearch for AI move
+            board = game.execute(aiMove, board);
+            printBoard(board);
+            x++;
+        }
+
+        System.out.println("Game over!");
+        int utility = game.utility(board);
+        if (utility > 0) {
+            System.out.println("AI wins!");
+        } else if (utility < 0) {
+            System.out.println("You win!");
+        } else {
+            System.out.println("It's a draw!");
+        }
+    }
+
+
+
+
+    public void printBoard(char[][] board) {
+        for (int row = 0; row < board.length; row++) {
+            for (int column = 0; column < board.length; column++) {
+                System.out.print(board[row][column]);
+                if (column < board.length - 1) {
+                    System.out.print("|");
+                }
+            }
+            System.out.println();
+            if (row < board.length - 1) {
+                for (int column = 0; column < board.length; column++) {
+                    System.out.print("-");
+                    if (column < board.length - 1) {
+                        System.out.print("+");
+                    }
+                }
+                System.out.println();
+            } else {
+                System.out.println();
+            }
+        }
     }
 
     public static void main(String[] args) {
-        TicTacToePlayer player = new TicTacToePlayer(3); // Choose size of board
+        TicTacToePlayer player = new TicTacToePlayer(3);
         player.play();
     }
 }
