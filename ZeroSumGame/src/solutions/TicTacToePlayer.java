e2adb2513e73c3f74a595434cd1e64718cab8b12
North-Kaster <bckaster@bsu.edu>
2024-03-27 10:02:04 -0400

    Add unversioned files

---
+++ ZeroSumGame/src/solutions/TicTacToePlayer.java
@@ -0,0 +1,9 @@
+package solutions;
+
+public class TicTacToePlayer {
+    //while we have not reached a terminal state:
+         //print the current board
+         //let the human player input (row, column)
+        //run the minimax search to find the best move
+    //announce the winner or draw
+}
